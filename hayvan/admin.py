39f2037c514e82651b1e3abb3f99f3bf02831688
TanerSB <babaoglutanersahin@gmail.com>
2022-02-05 19:15:06 +0300

    update

--- hayvan/admin.py
+++ hayvan/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from hayvan.models import Hayvan
 # Register your models here.
+
+class HayvanAdmin(admin.ModelAdmin):
+
+    class Meta:
+        model = Hayvan
+
+
+
+
+admin.site.register(Hayvan,HayvanAdmin)
--- hayvan/models.py
+++ hayvan/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Hayvan(models.Model):
+    tür = models.CharField(max_length=50,verbose_name='Türü')
+    cins = models.CharField(max_length=50,verbose_name='Cinsi')
+    isim = models.CharField(max_length=50,verbose_name='İsmi')
+    açıklama = models.TextField(verbose_name='Açıklaması')
+    dogumtarihi = models.DateField('Doğum Tarihi')
+
+    def __str__(self):
+        return self.tür
+
